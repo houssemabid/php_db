005dc102627d54464515212a51538b78894c31b6
houssemabid <houssem.abid@outlook.fr>
2023-12-04 15:00:07 +0100

    ajout produit

--- welcome.php
+++ welcome.php
@@ -1,8 +1,37 @@
 <html>
 <body>
 
-Welcome <?php echo $_POST["name"]; ?><br>
-Your email address is: <?php echo $_POST["email"]; ?>
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "stock";
 
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$name = $_POST["name"];
+$price = $_POST["price"];
+$quantity = $_POST["quantity"];
+
+$sql = "INSERT INTO article (name, price, quantity)
+VALUES ('".$name."',". $price.", ".$quantity.")";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
+
+
+Vous avez ajouté le produit: <?php echo $_POST["name"]; ?> avec prix <?php echo $_POST["price"]; ?> et quantité
+<?php echo $_POST["quantity"]; ?>
 </body>
 </html>
