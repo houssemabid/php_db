c5c650f813ccc8222ed014c097a39c8e4321d2c3
houssemabid <houssem.abid@outlook.fr>
2023-12-04 14:27:15 +0100

    first commit

---
+++ insert.php
@@ -0,0 +1,24 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "stock";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO article (name, price, quantity)
+VALUES ('cafe', 4000, 60)";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
